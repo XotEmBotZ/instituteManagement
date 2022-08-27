709a62e446f1b9d425ead2523ff8dda453a1fe30
XotEmBotZ <xotembotzop@gmail.com>
2022-08-27 19:40:47 +0530

    Minor changes

--- mainServer/api/apps.py
+++ mainServer/api/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+import os
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE') 
+        if run_once is not None:
+            return
+        os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True'
+
+        from . import bgJobs
+        bgJobs.run_continuously()
--- mainServer/api/bgJobs.py
+++ mainServer/api/bgJobs.py
@@ -1,3 +1,33 @@
+import sched
 from students import models as std_models
 from teachers import models as teachers_models
-import sche
+import schedule
+import threading
+import time
+
+def run_threaded(job_func,*args,**kwargs):
+    job_thread = threading.Thread(target=job_func,args=args,kwargs=kwargs)
+    job_thread.start()
+
+def run_continuously(interval=1):
+    cease_continuous_run = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+    return cease_continuous_run
+
+def printTest(Text):
+    time.sleep(10)
+
+schedule.every(10).seconds.do(run_threaded,printTest,Text="Texts")
+# #Start the background thread
+# stop_run_continuously = run_continuously()
+# # Stop the background thread
+# stop_run_continuously.set()
